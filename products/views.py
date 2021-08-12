a81636bfee68bdbe15188d5741d38f1ddf9b8ee1
nomad <nomadsarychev@yandex.ru>
2021-08-12 17:47:17 +0300

    исправил кавычки

--- products/views.py
+++ products/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # контроллеры
 
 def index(request):
-    return render(request, "products/index.html")
+    return render(request, 'products/index.html')
 
 def products(request):
     return render(request, 'products/products.html')
