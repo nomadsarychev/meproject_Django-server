a19d3ae312a1b105191fbf809b83b58c1a3c6384
nomad <nomadsarychev@yandex.ru>
2021-08-24 14:23:55 +0300

    добавлена логика контролера

--- users/views.py
+++ users/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import auth
+
+from users.form import UserLoginForm
 
 
 # Контролеры
 
 def login(request):
-    context = {'title': 'Geekshop - Авторизация'}
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+
+    form = UserLoginForm()
+    context = {'title': 'Geekshop - Авторизация', 'form': form}
+    return render(request, 'users/login.html', context)
 
 
 def registration(request):
     context = {'title': 'Geekshop - Регитсрация'}
-    return render(request, 'users/registration.html')
+    return render(request, 'users/registration.html', context)
