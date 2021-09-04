010cd28c09441abbe11308ad4827fc979017bdd7
nomad <nomadsarychev@yandex.ru>
2021-09-04 16:32:06 +0300

    контролер для чтение пользователей

--- admin_us/urls.py
+++ admin_us/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from admin_us.views import index
+from admin_us.views import index, admin_users
 
 app_name = 'admin_us'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('users/', admin_users, name='admin_users'),
 ]
--- admin_us/views.py
+++ admin_us/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 
+from users.models import User
+
 
 def index(request):
     context = {'title': 'GeekShop - Admin'}
     return render(request, 'admin_us/index.html', context)
+
+
+def admin_create(request):
+    context = {'title': 'GeekShop - Admin - create'}
+    return render(request, 'admin_us/create_user.html', context)
+
+
+def admin_users(request):
+    context = {'title': 'GeekShop - Admin - create',
+               'users': User.objects.all()}
+    return render(request, 'admin_us/admin_users.html', context)
