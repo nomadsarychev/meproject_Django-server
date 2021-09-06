f0a70daeb5b89886ecc8f1577eccbe9680348600
nomad <nomadsarychev@yandex.ru>
2021-09-06 16:53:59 +0300

    Работва с панелью админ

--- products/admin.py
+++ products/admin.py
@@ -5,5 +5,14 @@ from products.models import ProductCategory, Product
 
 # Их регистрация в админке.
 
-admin.site.register(Product)
+
 admin.site.register(ProductCategory)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity')
+    fields = ('name', 'image', 'description', ('price', 'quantity'), 'category')
+    readonly_fields = ('description',)
+    ordering = ('name',)
+    search_fields = ('name',)
