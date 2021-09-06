070a9b1764322fbecc561e86b6127fcb4fb1082d
nomad <nomadsarychev@yandex.ru>
2021-09-06 16:23:55 +0300

    добавление декоратора

--- admin_us/views.py
+++ admin_us/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import user_passes_test
 
 from users.models import User
 from admin_us.forms import UserRegistrationForm, UserAdminProfileForm
 
 
+@user_passes_test(lambda u: u.is_staff)
 def index(request):
     context = {'title': 'GeekShop - Admin'}
     return render(request, 'admin_us/index.html', context)
 
-
+@user_passes_test(lambda u: u.is_staff)
 def admin_create(request):
     if request.method == 'POST':
         form = UserRegistrationForm(data=request.POST, files=request.FILES)
@@ -21,13 +23,13 @@ def admin_create(request):
     context = {'title': 'GeekShop - Создание пользователя', 'form': form}
     return render(request, 'admin_us/admin_create.html', context)
 
-
+@user_passes_test(lambda u: u.is_staff)
 def admin_users(request):
     context = {'title': 'GeekShop - Пользователи',
                'users': User.objects.all()}
     return render(request, 'admin_us/admin_users.html', context)
 
-
+@user_passes_test(lambda u: u.is_staff)
 def admin_update(request, id):
     selected_user = User.objects.get(id=id)
     if request.method == 'POST':
@@ -40,9 +42,9 @@ def admin_update(request, id):
     context = {'title': 'GeekShop - Редактирвоание пользователя', 'selected_user': selected_user, 'form': form}
     return render(request, 'admin_us/admin_update.html', context)
 
-
+@user_passes_test(lambda u: u.is_staff)
 def admin_delete(request, id):
     user = User.objects.get(id=id)
-    user.is_active=False
+    user.is_active = False
     user.save()
     return HttpResponseRedirect(reverse('admin_us:admin_users'))
