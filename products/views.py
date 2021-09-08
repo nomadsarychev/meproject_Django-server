11af6da7b3ae40b88baa5241a88b7bd8f4c694fb
nomad <nomadsarychev@yandex.ru>
2021-09-08 23:24:31 +0300

    добавлена фильтрация по категориям

--- products/views.py
+++ products/views.py
@@ -10,9 +10,11 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request):
+def products(request, category_id=None):
     context = {'title': 'Geekshop-Каталог',
-               'products': Product.objects.all(),
                'categories': ProductCategory.objects.all(),
                }
+    products = Product.objects.filter(
+        category_id=category_id) if category_id else Product.objects.all()  # можно сразу записать в словарь
+    context['products'] = products
     return render(request, 'products/products.html', context)
