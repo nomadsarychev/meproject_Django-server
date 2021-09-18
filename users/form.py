05ed4f75fb905a1591b5fc5e83089574dc19ab88
nomad <nomadsarychev@yandex.ru>
2021-09-18 15:12:22 +0300

    активация пользователся спомощью email переходом по ссылке.

--- users/form.py
+++ users/form.py
@@ -37,10 +37,9 @@ class UserRegistrationForm(UserCreationForm):
 
     def save(self):
         user = super(UserRegistrationForm, self).save()
-
         user.is_active = False
         salt = hashlib.sha1(str(random.random()).encode('utf8')).hexdigest()[:6]
-        user.activation_key=hashlib.sha1((user.email+salt).encode('utf8')).hexdigest()
+        user.activation_key = hashlib.sha1((user.email + salt).encode('utf8')).hexdigest()
         user.save()
 
         return user
